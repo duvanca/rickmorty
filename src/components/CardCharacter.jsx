89503eaeffb4330df2bda271e56300027eef86a9
duvanca <73137805+duvanca@users.noreply.github.com>
2022-06-13 18:50:26 -0500

    error

--- src/components/CardCharacter.jsx
+++ src/components/CardCharacter.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import Loader from './loader'
+import Loader from './Loader'
 
 const CardCharacter = ({ resident }) => {
 
